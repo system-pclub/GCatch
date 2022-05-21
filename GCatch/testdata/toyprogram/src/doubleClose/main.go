e40cc6dd4cee929b2fc1593d7328037ed282ffdf
songlh <songlinhai0543@gmail.com>
2022-05-21 17:44:02 -0400

    add one new example

--- GCatch/testdata/toyprogram/src/doubleClose/main.go
+++ GCatch/testdata/toyprogram/src/doubleClose/main.go
@@ -1,6 +1,40 @@
 package main
 
+type client struct {
+	closed chan int
+}
+
+func newClient() *client {
+	var c *client
+	c = new(client)
+	c.closed = make(chan int)
+	return c
+}
+
+func (c *client) closeClient() {
+	select {
+	case <-c.closed:
+	default:
+		close(c.closed)
+	}
+}
+
+
+func test1() {
+	c := newClient()
+
+	go func() {
+		c.closeClient()
+	}()
+
+	go func() {
+		c.closeClient()
+	}()
+}
+
+
 func main() {
+	test1()
 	c1 := make(chan int)
 	go func() {
 		close(c1)
